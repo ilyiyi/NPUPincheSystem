7a4ff011dbdae2688a6d5c34e310e79efda5157c
一滴水～ <oncwnuAnal_ziGdwEG8z2l23dXYQ@git.weixin.qq.com>
2023-04-12 20:54:41 +0800

    构建项目基本框架

---
+++ PincheSystem/src/main/java/com/devhub/pinchesystem/PincheSystemApplication.java
@@ -0,0 +1,13 @@
+package com.devhub.pinchesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PincheSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PincheSystemApplication.class, args);
+    }
+
+}
---
+++ PincheSystem/src/test/java/com/devhub/pinchesystem/PincheSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhub.pinchesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PincheSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
