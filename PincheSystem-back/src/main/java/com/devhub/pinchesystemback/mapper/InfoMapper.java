4ae4556b0f3277f2ad4f967e1b650b060fe3722e
一滴水～ <oncwnuAnal_ziGdwEG8z2l23dXYQ@git.weixin.qq.com>
2023-05-08 22:39:57 +0800

    新增拼车信息的增、删、改、查功能

---
+++ PincheSystem-back/src/main/java/com/devhub/pinchesystemback/mapper/InfoMapper.java
@@ -0,0 +1,21 @@
+package com.devhub.pinchesystemback.mapper;
+
+import com.devhub.pinchesystemback.domain.Info;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface InfoMapper {
+    int deleteByPrimaryKey(Long infoId);
+
+    int insert(Info row);
+
+    Info selectByPrimaryKey(Long infoId);
+
+    List<Info> selectAll();
+
+    List<Info> selectByOwner(Long ownerId);
+
+    int updateByPrimaryKey(Info row);
+}
