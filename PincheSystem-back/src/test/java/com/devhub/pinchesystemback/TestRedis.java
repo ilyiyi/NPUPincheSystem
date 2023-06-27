7aff59f9168b8a9603c84b2e2bb336418a577e9b
wak <2231751148@qq.com>
2023-06-27 20:26:38 +0800

    完结撒花

--- PincheSystem-back/src/test/java/com/devhub/pinchesystemback/TestRedis.java
+++ PincheSystem-back/src/test/java/com/devhub/pinchesystemback/TestRedis.java
@@ -3,12 +3,10 @@ package com.devhub.pinchesystemback;
 import com.devhub.pinchesystemback.domain.User;
 import com.devhub.pinchesystemback.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.annotation.Resource;
-import java.util.*;
 
 /**
  * @author wak
